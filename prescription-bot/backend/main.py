5022599b745829296084619d591d8f64a8addc8e
Sai Vighnesh Ivaturi <viggy.ivaturi929@gmail.com>
2026-06-15 22:11:39 +0530

    Allow AWS frontend origin in CORS

--- prescription-bot/backend/main.py
+++ prescription-bot/backend/main.py
@@ -23,7 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+"http://43.204.111.61:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
